4e748726fa5451e58258a8255c79bb715b9f3824
MiguelRivera03 <miguel.angel.rivera@correounivalle.edu.co>
2024-09-24 19:36:08 -0500

    First proyect of Backend Specialist Course

---
+++ test-service/src/test/java/com/course/digitalHouse/test_service/TestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.digitalHouse.test_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
